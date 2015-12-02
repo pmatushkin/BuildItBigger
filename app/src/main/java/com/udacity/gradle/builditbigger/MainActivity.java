f09aaeba847edf9c8903ae4fa166885485cdde40
pmatushkin <pmatushkin@gmail.com>
2015-12-01 22:22:01 -0500

    Create a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeSmith;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeSmith jokeSmith = new JokeSmith();
+
+        Toast.makeText(this, jokeSmith.tellAHandCraftedJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javajokes/src/main/java/com/example/JokeSmith.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class JokeSmith {
+    public String tellAHandCraftedJoke(){
+        return "A hand-crafted joke";
+    }
+}
